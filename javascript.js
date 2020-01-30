387d60800a506731299dbe25d9f430db0e22688f
Erik Alfvin <erikalfvin@gmail.com>
2020-01-29 20:34:26 -0600

    Added function to handle API errors

--- javascript.js
+++ javascript.js
@@ -13,15 +13,13 @@ $(document).ready(function() {
 
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?APPID=10a0646374889cca48ebde2c7c4b3dcd&q=" + city;
 
-        console.log(queryURL);
-
         $.ajax({
             method: "GET",
-            url: queryURL
+            url: queryURL,
+            error: onError
         }).then(render);
 
         function render(response) {
-            console.log(response);
             var currentTemp = Math.round((response.main.temp - 273.15) * (9/5) + 32);
             var currentHumidity = response.main.humidity;
             var currentWind = response.wind.speed;
@@ -34,13 +32,21 @@ $(document).ready(function() {
             $("#Humidity").css("visibility", "visible");
             $("#Wind").css("visibility", "visible");
         }
+
+        function onError(a, b, c) {
+            $("#City").text(city + " - " + "City Not Found");
+            $("#Temperature").css("visibility", "hidden");
+            $("#Humidity").css("visibility", "hidden");
+            $("#Wind").css("visibility", "hidden");
+        }
+
         $.ajax({
             method: "GET",
             url:  "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=44&lon=-93&APPID=10a0646374889cca48ebde2c7c4b3dcd"
         }).then(renderUV);
 
         function renderUV(response) {
-            console.log(response);
+            //console.log(response);
         }
     }
 });
